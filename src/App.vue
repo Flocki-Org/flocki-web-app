d096bce99e41b20a74581502eb3c24b4df0f0d00
Mark Karimov <mark.karimov@gmail.com>
2022-06-20 14:47:53 +0200

    Add pug support

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,24 @@ import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
-<template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+<template lang="pug">
+header
+  img(
+    alt="Vue logo"
+    class="logo"
+    src="@/assets/logo.svg"
+    width="125"
+    height="125"
+  )
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  div(class="wrapper")
+    HelloWorld(msg="You did it!!!")
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    nav
+      RouterLink(to="/") Home
+      RouterLink(to="/about") About
 
-  <RouterView />
+RouterView
 </template>
 
 <style>
