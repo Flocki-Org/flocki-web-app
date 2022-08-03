c140f133c6c5897e61acdf1899b70e7ea23758a4
Mark Karimov <mark.karimov@gmail.com>
2022-08-03 22:33:07 +0200

    Complete Person page

--- src/views/People/PersonView.vue
+++ src/views/People/PersonView.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+onMounted(() => {
+  let map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+  });
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+  }).addTo(map);
+})
+</script>
+
 <template lang="pug">
 #person.ml-menu.py-main-tb
   .flex.items-center.px-main-lr.mb-10.relative
@@ -24,9 +42,9 @@
           d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z"
         )
 
-  .flex.px-main-lr.gap-4
-    .flex.flex-col.gap-4(class="w-3/5")
-      .rounded-lg.bg-white(
+  .flex.px-main-lr.gap-5
+    .flex.flex-col.gap-5(class="w-3/5")
+      .rounded-lg.bg-white.relative.overflow-hidden(
         class="shadow-[0_1px_3px_rgba(0,0,0,0.1)] hover_shadow-[0_2px_6px_rgba(0,0,0,0.1)]"
       )
         .flex.items-center.px-10.py-8
@@ -45,22 +63,22 @@
         table.table-auto.w-full.text-left.text-sm
           tbody
             tr.odd_bg-gray-50.even_bg-white
-              td.py-4.pl-10.text-gray-500 Date of birth
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Date of birth
               td.py-4 12 November 1968 (53 years old)
             tr.odd_bg-gray-50.even_bg-white
-              td.py-4.pl-10.text-gray-500 Gender
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Gender
               td.py-4 Male
             tr.odd_bg-gray-50.even_bg-white
-              td.py-4.pl-10.text-gray-500 Marital status
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Marital status
               td.py-4 Married (19 March 1993 - 29 years)
             tr.odd_bg-gray-50.even_bg-white
-              td.py-4.pl-10.text-gray-500 Email address
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Email address
               td.py-4 andrew.levinsohn@gmail.com
             tr.odd_bg-gray-50.even_bg-white
-              td.py-4.pl-10.text-gray-500 Phone number
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Phone number
               td.py-4 0815972198
             tr.odd_bg-gray-50.even_bg-white
-              td.py-4.pl-10.text-gray-500 Social profiles
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Social profiles
               td.py-4.flex
                 a.mr-1.hover_opacity-80.transition-all(
                   href="#"
@@ -102,10 +120,235 @@
               td.py-4.pl-10.text-gray-500 Registered at
               td.py-4 12 December 2021, 08:48pm
 
-      .rounded-lg.bg-white
+      .rounded-lg.bg-white.relative.overflow-hidden(
+        class="shadow-[0_1px_3px_rgba(0,0,0,0.1)] hover_shadow-[0_2px_6px_rgba(0,0,0,0.1)]"
+      )
+        .flex.items-center.px-10.py-8
+          h2.grow.text-gray-700 Church Life
 
-    div(class="w-2/5")
+          button.w-8.h-8.inline-flex.justify-center.items-center.rounded-md.border.border-gray-300.hover_border-gray-200.text-gray-400.bg-transparent.hover_bg-gray-50.transition-all
+            svg(
+              style="width:16px;height:16px"
+              viewBox="0 0 24 24"
+            )
+              path(
+                fill="currentColor"
+                d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+              )
+
+        table.table-auto.w-full.text-left.text-sm
+          tbody
+            tr.odd_bg-gray-50.even_bg-white
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Role
+              td.py-4 Member, Deacon
+            tr.odd_bg-gray-50.even_bg-white
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Date baptized
+              td.py-4 25 February 1992
+            tr.odd_bg-gray-50.even_bg-white
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Member from
+              td.py-4 30 June 1991
+            tr.odd_bg-gray-50.even_bg-white
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Leading ministries
+              td.py-4
+                ul
+                  li
+                    router-link(to="#") Hospitality Ministry
+                  li
+                    router-link(to="#") Web & IT Ministry
+            tr.odd_bg-gray-50.even_bg-white
+              td.py-4.pl-10.text-gray-500(class="w-2/5") Groups
+              td.py-4
+                ul
+                  li
+                    router-link(to="#") The Book Club
+
+      .rounded-lg.bg-white.relative.overflow-hidden(
+        class="shadow-[0_1px_3px_rgba(0,0,0,0.1)] hover_shadow-[0_2px_6px_rgba(0,0,0,0.1)]"
+      )
+        .flex.items-center.px-10.py-8
+          h2.grow.text-gray-700 Address
+
+          button.w-8.h-8.inline-flex.justify-center.items-center.rounded-md.border.border-gray-300.hover_border-gray-200.text-gray-400.bg-transparent.hover_bg-gray-50.transition-all
+            svg(
+              style="width:16px;height:16px"
+              viewBox="0 0 24 24"
+            )
+              path(
+                fill="currentColor"
+                d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+              )
+
+        .address.px-10.mb-4.text-gray-500.text-sm 27 Tate cres., Lonehill, Johannesburg, 2062
+
+        #map.w-full.h-60
+
+      .rounded-lg.bg-white.relative.overflow-hidden(
+        class="shadow-[0_1px_3px_rgba(0,0,0,0.1)] hover_shadow-[0_2px_6px_rgba(0,0,0,0.1)]"
+      )
+        .flex.items-center.px-10.py-8
+          h2.grow.text-gray-700 Prayer Requests
+
+          button.w-8.h-8.inline-flex.justify-center.items-center.rounded-md.border.border-gray-300.hover_border-gray-200.text-gray-400.bg-transparent.hover_bg-gray-50.transition-all
+            svg(
+              style="width:16px;height:16px"
+              viewBox="0 0 24 24"
+            )
+              path(
+                fill="currentColor"
+                d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+              )
+
+        .text-sm.font-bold.text-gray-500.px-10 27 February 2022
+        .text-sm.text-gray-500.px-10.mb-6 Please pray for my grandmother that she may be saved, pray for my family for strength and comfort in this difficult time.
+
+    .flex.flex-col.gap-5(class="w-2/5")
       img.rounded-lg.w-full(
         src="@/assets/temp/person-andrew-levinsohn.jpg"
       )
+
+      .rounded-lg.bg-white.relative.overflow-hidden(
+        class="shadow-[0_1px_3px_rgba(0,0,0,0.1)] hover_shadow-[0_2px_6px_rgba(0,0,0,0.1)]"
+      )
+        .flex.items-center.px-10.py-8
+          h2.grow.text-gray-700 Levinsohn Household
+
+          button.w-8.h-8.inline-flex.justify-center.items-center.rounded-md.border.border-gray-300.hover_border-gray-200.text-gray-400.bg-transparent.hover_bg-gray-50.transition-all
+            svg(
+              style="width:16px;height:16px"
+              viewBox="0 0 24 24"
+            )
+              path(
+                fill="currentColor"
+                d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+              )
+
+        img.w-full(
+          src="@/assets/temp/levinsohn-household.jpg"
+        )
+
+        div
+          .flex.shrink.items-center.p-4.gap-3.odd_bg-gray-50.even_bg-white
+            img.rounded-lg(
+              src="@/assets/temp/levinsohn-household-andrew.jpg"
+              width="82"
+            )
+            .flex.flex-col
+              .inline-flex.items-center(
+                title="Primary contact"
+              )
+                router-link.mr-2(
+                  to="#"
+                ) Andrew Levinsohn
+                svg.text-sky-500(
+                  style="width:16px;height:16px"
+                  viewBox="0 0 24 24"
+                )
+                  path(
+                    fill="currentColor"
+                    d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
+                  )
+              .text-sm.text-gray-400 Male, 53 years old
+              .inline-flex.items-center
+                svg.mr-1.text-gray-400(
+                  style="width:14px;height:14px"
+                  viewBox="0 0 24 24"
+                )
+                  path(
+                    fill="currentColor"
+                    d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"
+                  )
+                .text-sm.text-gray-400 0815972198
+              .inline-flex.items-center
+                svg.mr-1.text-gray-400(
+                  style="width:14px;height:14px"
+                  viewBox="0 0 24 24"
+                )
+                  path(
+                    fill="currentColor"
+                    d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
+                  )
+                .text-sm.text-gray-400 andrew.levinsohn@gmail.com
+
+          .flex.shrink.items-center.p-4.gap-3.odd_bg-gray-50.even_bg-white
+            img.rounded-lg(
+              src="@/assets/temp/levinsohn-household-natasha.jpg"
+              width="82"
+            )
+            .flex.flex-col
+              .inline-flex.items-center
+                router-link.mr-2(
+                  to="#"
+                ) Natasha Levinsohn
+              .text-sm.text-gray-400 Female, 48 years old
+              .inline-flex.items-center
+                svg.mr-1.text-gray-400(
+                  style="width:14px;height:14px"
+                  viewBox="0 0 24 24"
+                )
+                  path(
+                    fill="currentColor"
+                    d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"
+                  )
+                .text-sm.text-gray-400 0792390181
+              .inline-flex.items-center
+                svg.mr-1.text-gray-400(
+                  style="width:14px;height:14px"
+                  viewBox="0 0 24 24"
+                )
+                  path(
+                    fill="currentColor"
+                    d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
+                  )
+                .text-sm.text-gray-400 nlevinsohn21@gmail.com
+
+          .flex.shrink.items-center.p-4.gap-3.odd_bg-gray-50.even_bg-white
+            img.rounded-lg(
+              src="@/assets/temp/levinsohn-household-leah.jpg"
+              width="82"
+            )
+            .flex.flex-col
+              .inline-flex.items-center
+                router-link.mr-2(
+                  to="#"
+                ) Leah Levinsohn
+              .text-sm.text-gray-400 Female, 10 years old
+              .inline-flex.items-center
+                svg.mr-1.text-gray-400(
+                  style="width:14px;height:14px"
+                  viewBox="0 0 24 24"
+                )
+                  path(
+                    fill="currentColor"
+                    d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"
+                  )
+                .text-sm.text-gray-400 0793982286
+              .inline-flex.items-center
+                svg.mr-1.text-gray-400(
+                  style="width:14px;height:14px"
+                  viewBox="0 0 24 24"
+                )
+                  path(
+                    fill="currentColor"
+                    d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
+                  )
+                .text-sm.text-gray-400 leah.levinsohn@gmail.com
+
+      .rounded-lg.bg-white.relative.overflow-hidden(
+        class="shadow-[0_1px_3px_rgba(0,0,0,0.1)] hover_shadow-[0_2px_6px_rgba(0,0,0,0.1)]"
+      )
+        .flex.items-center.px-10.py-8
+          h2.grow.text-gray-700 Private Notes
+
+          button.w-8.h-8.inline-flex.justify-center.items-center.rounded-md.border.border-gray-300.hover_border-gray-200.text-gray-400.bg-transparent.hover_bg-gray-50.transition-all
+            svg(
+              style="width:16px;height:16px"
+              viewBox="0 0 24 24"
+            )
+              path(
+                fill="currentColor"
+                d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+              )
+
+        .text-sm.font-bold.text-gray-500.px-10 All Stewards only
+        .text-sm.text-gray-500.px-10.mb-6 Haven’t been to church in 2 months. Need to check in.
 </template>
