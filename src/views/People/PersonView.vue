2399e5d7a711ecb0f6571575d4e91656d9da59a8
Pillay Rowan <qxz3dj9@africa.bmw.corp>
2023-09-19 16:31:03 +0200

    Remove debugging statements

--- src/views/People/PersonView.vue
+++ src/views/People/PersonView.vue
@@ -32,14 +32,9 @@ const loadPerson = (id) => {
 
       useRepo(Person).save(response.data)
       person.value = useRepo(Person).find(+id)
-      console.log("Person: ");
-      console.log(person.value);
       // TODO: remove timeout later
       setTimeout(() => {
         isLoadingPerson.value = false
-        console.log(person.householdsArray)
-
-        console.log('ending IsLoadPerson')
       }, 2000)
     })
     .catch((err) => {
@@ -283,121 +278,13 @@ onMounted(() => {
                 </template>
               </template>
             </template>
-            <template v-else>
+            <!--template v-else>
               <p>Got here 1</p>
             </template>
             <template v-if="!person">
               <p>Got here 2</p>
-            </template>
+            </template-->
           </div>
-          <!--div
-          .flex.shrink.items-center.p-4.gap-3.odd_bg-gray-50.even_bg-white
-            img.rounded-lg(
-              src="@/assets/temp/levinsohn-household-andrew.jpg"
-              width="82"
-            )
-            .flex.flex-col
-              .inline-flex.items-center(
-                title="Primary contact"
-              )
-                router-link.mr-2(
-                  to="#"
-                ) Andrew Levinsohn
-                svg.text-sky-500(
-                  style="width:16px;height:16px"
-                  viewBox="0 0 24 24"
-                )
-                  path(
-                    fill="currentColor"
-                    d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
-                  )
-              .text-sm.text-gray-400 Male, 53 years old
-              .inline-flex.items-center
-                svg.mr-1.text-gray-400(
-                  style="width:14px;height:14px"
-                  viewBox="0 0 24 24"
-                )
-                  path(
-                    fill="currentColor"
-                    d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"
-                  )
-                .text-sm.text-gray-400 0815972198
-              .inline-flex.items-center
-                svg.mr-1.text-gray-400(
-                  style="width:14px;height:14px"
-                  viewBox="0 0 24 24"
-                )
-                  path(
-                    fill="currentColor"
-                    d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
-                  )
-                .text-sm.text-gray-400 andrew.levinsohn@gmail.com
-          
-          .flex.shrink.items-center.p-4.gap-3.odd_bg-gray-50.even_bg-white
-            img.rounded-lg(
-              src="@/assets/temp/levinsohn-household-natasha.jpg"
-              width="82"
-            )
-            .flex.flex-col
-              .inline-flex.items-center
-                router-link.mr-2(
-                  to="#"
-                ) Natasha Levinsohn
-              .text-sm.text-gray-400 Female, 48 years old
-              .inline-flex.items-center
-                svg.mr-1.text-gray-400(
-                  style="width:14px;height:14px"
-                  viewBox="0 0 24 24"
-                )
-                  path(
-                    fill="currentColor"
-                    d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"
-                  )
-                .text-sm.text-gray-400 0792390181
-              .inline-flex.items-center
-                svg.mr-1.text-gray-400(
-                  style="width:14px;height:14px"
-                  viewBox="0 0 24 24"
-                )
-                  path(
-                    fill="currentColor"
-                    d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
-                  )
-                .text-sm.text-gray-400 nlevinsohn21@gmail.com
-          
-          .flex.shrink.items-center.p-4.gap-3.odd_bg-gray-50.even_bg-white
-            img.rounded-lg(
-              src="@/assets/temp/levinsohn-household-leah.jpg"
-              width="82"
-            )
-            .flex.flex-col
-              .inline-flex.items-center
-                router-link.mr-2(
-                  to="#"
-                ) Leah Levinsohn
-              .text-sm.text-gray-400 Female, 10 years old
-              .inline-flex.items-center
-                svg.mr-1.text-gray-400(
-                  style="width:14px;height:14px"
-                  viewBox="0 0 24 24"
-                )
-                  path(
-                    fill="currentColor"
-                    d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"
-                  )
-                .text-sm.text-gray-400 0793982286
-              .inline-flex.items-center
-                svg.mr-1.text-gray-400(
-                  style="width:14px;height:14px"
-                  viewBox="0 0 24 24"
-                )
-                  path(
-                    fill="currentColor"
-                    d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
-                  )
-                .text-sm.text-gray-400 leah.levinsohn@gmail.com
-          
-          -->
         </div>
         <div class="rounded-lg bg-white relative overflow-hidden shadow-[0_1px_3px_rgba(0,0,0,0.1)] hover_shadow-[0_2px_6px_rgba(0,0,0,0.1)]">
           <div class="flex items-center px-10 py-8">
